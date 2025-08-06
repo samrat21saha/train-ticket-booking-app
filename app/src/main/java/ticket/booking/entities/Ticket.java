e9a9d54e7fa67d4fb83d7e30320044ce3ad20d05
Samrat Saha <140338992+Samsgithub9635@users.noreply.github.com>
2025-08-06 23:26:55 +0530

    initial commit

---
+++ app/src/main/java/ticket/booking/entities/Ticket.java
@@ -0,0 +1,13 @@
+package ticket.booking.entities;
+
+import java.util.Date;
+
+public class Ticket {
+    private String ticketId;
+    private String userId;
+    private String sourceStation;
+    private String destinationStation;
+    private Date dateOfTravel;
+    private Train train;
+
+}
