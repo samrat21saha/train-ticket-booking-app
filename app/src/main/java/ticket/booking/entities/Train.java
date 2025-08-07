3b844ab09d48b79c32604f1bea08b80d6c1220cf
Samrat Saha <140338992+Samsgithub9635@users.noreply.github.com>
2025-08-07 20:20:55 +0530

    created: getter and setter methods to access all the details of the train

--- app/src/main/java/ticket/booking/entities/Train.java
+++ app/src/main/java/ticket/booking/entities/Train.java
@@ -11,4 +11,43 @@ public class Train {
     private Map<String, Time> stationArrivalTimes;
     private List<String> stations;
 
+    public String getTrainId() {
+        return trainId;
+    }
+
+    public void setTrainId(String trainId) {
+        this.trainId = trainId;
+    }
+
+    public String getTrainNo() {
+        return trainNo;
+    }
+
+    public void setTrainNo(String trainNo) {
+        this.trainNo = trainNo;
+    }
+
+    public List<List<Integer>> getSeats() {
+        return seats;
+    }
+
+    public void setSeats(List<List<Integer>> seats) {
+        this.seats = seats;
+    }
+
+    public Map<String, Time> getStationArrivalTimes() {
+        return stationArrivalTimes;
+    }
+
+    public void setStationArrivalTimes(Map<String, Time> stationArrivalTimes) {
+        this.stationArrivalTimes = stationArrivalTimes;
+    }
+
+    public List<String> getStations() {
+        return stations;
+    }
+
+    public void setStations(List<String> stations) {
+        this.stations = stations;
+    }
 }
