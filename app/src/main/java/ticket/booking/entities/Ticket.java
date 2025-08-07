041296fffd982db67fcb3536bbbf81a24041a224
Samrat Saha <140338992+Samsgithub9635@users.noreply.github.com>
2025-08-07 20:25:31 +0530

    created: getter and setters and a getTicketInfo() method that will return all the details of the ticket which will be accessed by printTickets() method in Uer class to print the ticket booked by the user with user and train details in it

--- app/src/main/java/ticket/booking/entities/Ticket.java
+++ app/src/main/java/ticket/booking/entities/Ticket.java
@@ -7,7 +7,69 @@ public class Ticket {
     private String userId;
     private String sourceStation;
     private String destinationStation;
-    private Date dateOfTravel;
+    private String dateOfTravel;
     private Train train;
 
+    public Ticket(){}
+
+    public Ticket(String ticketId, String userId, String source, String destination, String dateOfTravel, Train train){
+        this.ticketId = ticketId;
+        this.userId = userId;
+        this.sourceStation = source;
+        this.destinationStation = destination;
+        this.dateOfTravel = dateOfTravel;
+        this.train = train;
+    }
+
+    public String getTicketId() {
+        return ticketId;
+    }
+
+    public void setTicketId(String ticketId) {
+        this.ticketId = ticketId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getSourceStation() {
+        return sourceStation;
+    }
+
+    public void setSourceStation(String sourceStation) {
+        this.sourceStation = sourceStation;
+    }
+
+    public String getDestinationStation() {
+        return destinationStation;
+    }
+
+    public void setDestinationStation(String destinationStation) {
+        this.destinationStation = destinationStation;
+    }
+
+    public String getDateOfTravel() {
+        return dateOfTravel;
+    }
+
+    public void setDateOfTravel(String dateOfTravel) {
+        this.dateOfTravel = dateOfTravel;
+    }
+
+    public Train getTrain() {
+        return train;
+    }
+
+    public void setTrain(Train train) {
+        this.train = train;
+    }
+
+    public String getTicketInfo() {
+        return String.format("Ticket ID: %s belongs to User %s from %s to %s on %s", this.ticketId, this.userId, this.sourceStation, this.destinationStation, this.dateOfTravel);
+    }
 }
