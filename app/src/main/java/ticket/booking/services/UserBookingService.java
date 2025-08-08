f24a5370382bfd6fe4087b31d2d9b4fe5e7acd4d
Samrat Saha <140338992+Samsgithub9635@users.noreply.github.com>
2025-08-08 23:06:19 +0530

    feat: Implement user signup and login functionality

--- app/src/main/java/ticket/booking/services/UserBookingService.java
+++ app/src/main/java/ticket/booking/services/UserBookingService.java
@@ -3,10 +3,12 @@ package ticket.booking.services;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import ticket.booking.entities.User;
+import ticket.booking.util.UserServiceUtil;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 public class UserBookingService {
 
@@ -18,11 +20,48 @@ public class UserBookingService {
 
     private static final String USERS_PATH = "app/src/main/java/ticket/booking/localDb/users.json";
 
-    public UserBookingService(User user1) throws IOException {
-        this.user = user1;
+    public UserBookingService(User user) throws IOException {
+        this.user = user;
+        loadUserListFromFile();
+    }
+
+    public UserBookingService() throws IOException {
+        loadUserListFromFile();
+    }
+
+    private void loadUserListFromFile() throws IOException{
         File userDataFilePath = new File(USERS_PATH);
         userList = objectMapper.readValue(userDataFilePath, new TypeReference<List<User>>() {
         });
     }
 
+    public boolean loginUser(){
+        Optional<User> foundUser = userList.stream().filter(user1 ->{
+            return user1.getUserName().equals(user.getUserName()) && UserServiceUtil.checkPassword(user.getPassword(), user1.getHashedPassword());
+        }).findFirst();
+        return  foundUser.isPresent();
+    }
+
+    public  boolean signUp(User newUserData) throws  IOException {
+        userList.add(newUserData);
+        saveUserListToDataFile();
+        return true;
+    }
+
+    private void saveUserListToDataFile() throws IOException {
+        File updatedUserData = new File(USERS_PATH);
+        objectMapper.writeValue(updatedUserData, userList);
+    }
+
+
+    public void fetchBookings() {
+        Optional<User> userBooking = userList.stream().filter(userBookingData -> {
+            return userBookingData.getUserName().equals(user.getUserName()) && UserServiceUtil.checkPassword(user.getPassword(), userBookingData.getHashedPassword());
+        }).findFirst();
+        if (userBooking.isPresent()) {
+            userBooking.get().printTickets();
+        }
+    }
 }
+
+
