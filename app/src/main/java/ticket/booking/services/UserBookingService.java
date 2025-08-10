7e39a99fc006b4caa3bed6214182b5cf741e86fb
Samrat Saha <140338992+Samsgithub9635@users.noreply.github.com>
2025-08-10 23:32:39 +0530

    Implement train search, seat fetching, and booking logic in UserService

--- app/src/main/java/ticket/booking/services/UserBookingService.java
+++ app/src/main/java/ticket/booking/services/UserBookingService.java
@@ -3,11 +3,13 @@ package ticket.booking.services;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import ticket.booking.entities.Ticket;
+import ticket.booking.entities.Train;
 import ticket.booking.entities.User;
 import ticket.booking.util.UserServiceUtil;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.Scanner;
@@ -67,18 +69,18 @@ public class UserBookingService {
 
 
     // Booking Cancel method
-    public Boolean cancelBooking(String ticketId){
+    public Boolean cancelBooking(String ticketId) {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter your ticketId:");
         ticketId = in.next();
 
-        if(ticketId == null || ticketId.isEmpty()){
+        if (ticketId == null || ticketId.isEmpty()) {
             System.out.println("TicketId can't be empty:");
             return Boolean.FALSE;
-        }else{
+        } else {
             System.out.println("Are you Sure you want to cancel ticket with id:" + ticketId + "?");
             String cancelDecision = in.nextLine().toLowerCase();
-            switch (cancelDecision){
+            switch (cancelDecision) {
                 case "yes":
                     System.out.println("Ok Cancelling ticket with id:" + ticketId + "...");
                     break;
@@ -110,9 +112,44 @@ public class UserBookingService {
             System.out.println("No ticket found with ID " + ticketId);
             return Boolean.FALSE;
         }
-
-
     }
+
+
+    public List<Train> getTrains(String source, String destination){
+        try{
+            TrainService trainService = new TrainService();
+            return  trainService.searchTrains(source, destination);
+        }catch(IOException ex){
+                return new ArrayList<>();
+        }
+    }
+
+    public List<List<Integer>> fetchSeats(Train train){
+        return train.getSeats();
+    }
+
+    public Boolean bookTrainSeat(Train train, int row, int seat) {
+        try{
+            TrainService trainService = new TrainService();
+            List<List<Integer>> seats = train.getSeats();
+            if (row >= 0 && row < seats.size() && seat >= 0 && seat < seats.get(row).size()) {
+                if (seats.get(row).get(seat) == 0) {
+                    seats.get(row).set(seat, 1);
+                    train.setSeats(seats);
+                    trainService.addTrain(train);
+                    return true; // Booking successful
+                } else {
+                    return false; // Seat is already booked
+                }
+            } else {
+                return false; // Invalid row or seat index
+            }
+        }catch (IOException ex){
+            return Boolean.FALSE;
+        }
+    }
+
+
 }
 
 
